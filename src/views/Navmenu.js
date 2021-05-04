84c4a39eab57b2cdaf8c371bcb17eea547a1e702
Kaarthik Sundar <kaarthik01sundar@gmail.com>
2021-05-04 07:21:17 -0600

    Modifies experience, adds home

--- src/views/Navmenu.js
+++ src/views/Navmenu.js
@@ -11,6 +11,7 @@ module.exports = {
     view: function (vnode) {
 
         var links = [
+            new Link('Home', '/#!'),
             new Link('Research', '/#!/research'),
             new Link('Education', '/#!/education'),
             new Link('Experience', '/#!/experience'),
