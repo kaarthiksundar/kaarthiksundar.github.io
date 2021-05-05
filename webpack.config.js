dffdda8f4c289fa635c940be35abf3edcb2d7677
Kaarthik Sundar <kaarthik01sundar@gmail.com>
2021-05-04 20:08:52 -0600

    remove browser-sync dep

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,27 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
     main: path.resolve(__dirname, './src/index.js'),
   },
+
   output: {
     path: path.resolve(__dirname, './bin'),
     filename: 'app.js',
   },
+
+  devServer: {
+    port: 8080
+  },
+
+  // https://webpack.js.org/concepts/plugins/
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './index.html',
+      inject: true,
+      chunks: ['index'],
+      filename: 'index.html'
+    })
+  ]
 }
