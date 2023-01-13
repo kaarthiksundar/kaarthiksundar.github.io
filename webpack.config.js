df2985a092117a48e3d2f380fb9731a27a5ba345
Kaarthik Sundar <kaarthik01sundar@gmail.com>
2023-01-13 13:02:11 -0700

    upgrade dev dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,6 @@
 const path = require('path');
 
 module.exports = {
-  entry: {
-    main: path.resolve(__dirname, './src/index.js'),
-  },
 
   output: {
     path: path.resolve(__dirname, './bin'),
