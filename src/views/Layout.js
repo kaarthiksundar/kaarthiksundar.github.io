da24b4b752ae4e008e78ab3910d10e2bcb3ede35
Kaarthik Sundar <kaarthik01sundar@gmail.com>
2023-05-04 17:01:06 -0600

    fira sans font

--- src/views/Layout.js
+++ src/views/Layout.js
@@ -17,7 +17,7 @@ var view = function (vnode) {
         [content]);
 
     return m('div',
-        { class: 'andika' },
+        { class: 'firasans' },
         [m(Header), body, m(Footer)]
         );
 };
